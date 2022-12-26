447d0858d494e46a394d1feb1041787d301ae232
Samuel E. Conrado <samuelconrado59@gmail.com>
2022-12-26 20:45:52 -0300

    Delete simulation directory

--- simulation/qsim/work/comparador_vlg_sample_tst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity comparador_vlg_sample_tst is
-    port(
-        input_minus     : in     vl_logic_vector(7 downto 0);
-        input_plus      : in     vl_logic_vector(7 downto 0);
-        sampler_tx      : out    vl_logic
-    );
-end comparador_vlg_sample_tst;
--- simulation/qsim/work/portadora_vlg_check_tst/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity portadora_vlg_check_tst is
-    port(
-        wave            : in     vl_logic_vector(7 downto 0);
-        sampler_rx      : in     vl_logic
-    );
-end portadora_vlg_check_tst;
